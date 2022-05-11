b77268b6792ae5048ef32fcf9237d6957aaacafb
권유리 <99jiyusu@gmail.com>
2022-05-12 00:20:28 +0900

    Update: Vue2 프로젝트 재생성

---
+++ coala-frontend/src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <section>
+            <b-button @click="clickMe">Click Me</b-button>
+        </section>
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+    name: 'HomeView',
+    components: {
+        HelloWorld
+    },
+    methods: {
+        clickMe() {
+            this.$buefy.notification.open('Clicked!!');
+        }
+    }
+};
+</script>
