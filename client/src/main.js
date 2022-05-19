14ccb296cf7054910edc801447d06bb0dd717521
권유리 <99jiyusu@gmail.com>
2022-05-19 19:13:11 +0900

    Change: navbar 구성

--- client/src/main.js
+++ client/src/main.js
@@ -1,19 +1,31 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import Buefy from 'buefy';
-import 'buefy/dist/buefy.css';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+// require('@/assets/main.scss');
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+// 사용할 아이콘 등록(camelCase로)
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+library.add(faBell, faCircleUser);
+// library.add(faCircleUser);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.use(Buefy, {
-    defaultIconPack: 'fas',
-    defaultContainerElement: '#content'
-    // ...
+  defaultIconPack: "fas",
+  defaultContainerElement: "#content"
+  // ...
 });
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: (h) => h(App)
-}).$mount('#app');
+  router,
+  store,
+  render: (h) => h(App)
+}).$mount("#app");
