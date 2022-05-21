d7516feebe65ff20c3110576c4bacd48f9c9cb46
권유리 <99jiyusu@gmail.com>
2022-05-22 04:49:46 +0900

    ADD: 마이페이지 1차 개발

---
+++ client/src/components/RadarChartView.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <Radar
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height" />
+  </div>
+</template>
+
+<script>
+import { Radar } from "vue-chartjs/legacy";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  LineElement,
+  RadialLinearScale,
+  Filler
+} from "chart.js";
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  PointElement,
+  RadialLinearScale,
+  LineElement,
+  Filler
+);
+
+export default {
+  name: "RadarChart",
+  components: { Radar },
+  props: {
+    chartId: {
+      type: String,
+      default: "radar-chart"
+    },
+    datasetIdKey: {
+      type: String,
+      default: "label"
+    },
+    width: {
+      type: Number,
+      default: 270
+    },
+    height: {
+      type: Number,
+      default: 270
+    },
+    cssClasses: {
+      default: "",
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {}
+    },
+    plugins: {
+      type: Array,
+      default: () => []
+    }
+  },
+  data() {
+    return {
+      chartData: {
+        labels: [
+          "DP",
+          "그래프",
+          "그리디",
+          "정렬",
+          "자료구조",
+          "문자열",
+          "구현"
+        ],
+        datasets: [
+          {
+            label: "내 실력",
+            fill: true,
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgb(255, 99, 132)",
+            pointBackgroundColor: "rgb(255, 99, 132)",
+            pointBorderColor: "#fff",
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "rgba(255,99,132,1)",
+            data: [38, 48, 40, 79, 59, 27, 70]
+          }
+        ]
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    };
+  }
+};
+</script>
---
+++ client/src/views/MyPageView.vue
@@ -0,0 +1,105 @@
+<template>
+  <div>
+    <navbar-view />
+    <div class="container" style="padding-left: 100px; padding-right: 100px">
+      <div class="notification" style="height: 260px; padding-top: 40px">
+        <div style="float: left" class="profile-box">
+          <img
+            src="@/assets/profile.png"
+            alt="마이페이지 사진"
+            class="profile-img" />
+        </div>
+        <div style="margin-left: 100px">
+          <!-- <div class="profile-name">코알라</div>
+          <div class="profile-major">소프트웨어학부</div> -->
+          <p style="height: 70px">
+            <span class="profile-name">코알라 님</span>
+            <span class="profile-major">소프트웨어학부</span>
+          </p>
+          <p>
+            <span class="profile-name"
+              >랭크
+              <font-awesome-icon icon="fa-medal" size="1x" />
+            </span>
+            <span class="profile-major">123 문제 해결</span>
+          </p>
+          <!-- <span class="profile-name">코알라 님</span>
+          <span class="profile-major">소프트웨어학부</span> -->
+        </div>
+        <div style="float: right; margin-top: -100px">
+          <a href="/">
+            <font-awesome-icon icon="fa-gear" size="2x" />
+          </a>
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div
+      class="container"
+      style="padding-left: 100px; padding-right: 100px; margin-top: 50px">
+      <div class="notification" style="height: 400px; padding-top: 40px">
+        <h2>내 실력 보기?</h2>
+        <radar-chart />
+      </div>
+    </div>
+    <div
+      class="container"
+      style="padding-left: 100px; padding-right: 100px; margin-top: 50px">
+      <div class="notification" style="height: 400px; padding-top: 40px">
+        <h2>유형별 랭크</h2>
+        <rank-table />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import NavbarView from "../components/navbarView.vue";
+import RadarChart from "../components/RadarChartView.vue";
+import RankTable from "../components/RankTableView.vue";
+
+export default {
+  components: { NavbarView, RadarChart, RankTable },
+  data() {
+    return {
+      sampleData: ""
+    };
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+};
+</script>
+<style scoped>
+.profile-box {
+  width: 180px;
+  height: 180px;
+  border-radius: 70%;
+  overflow: hidden;
+}
+
+.profile-img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+
+.profile-name {
+  float: left;
+  text-align: left;
+  font-size: 1.8rem;
+  padding-left: 100px;
+  /* margin-top: 40px; */
+  /* padding-bottom: 40px; */
+}
+
+.profile-major {
+  /* text-align: left; */
+  font-size: 1rem;
+  padding-top: 10px;
+  /* margin-top: 40px; */
+  padding-right: 200px;
+  /* padding-bottom: 40px; */
+}
+</style>
