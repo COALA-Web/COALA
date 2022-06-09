5b9fa7b157b6cea1b24137d285ffec274f52a445
권유리 <99jiyusu@gmail.com>
2022-06-10 02:39:54 +0900

    Change: 기존 설문조사 페이지 비활성화

--- client/src/views/StartPageView.vue
+++ client/src/views/StartPageView.vue
@@ -13,16 +13,15 @@
 
       <!-- 레벨 체크 안내 -->
       <div>
-        <p>
+        <p style="margin-bottom: 40px">
           알고리즘 실력을 진단한 뒤 <br />
           지금 바로 학습을 시작해보세요!
         </p>
-        <!--  -->
-        <div class="buttons">
+        <!-- <div class="buttons">
           <b-button type="is-primary" @click="levelCheck()"
             >기존 레벨체크: 없애기</b-button
           >
-        </div>
+        </div> -->
 
         <b-button
           label="레벨 체크 시작하기"
@@ -49,8 +48,8 @@
 
       <!-- 서비스 소개 -->
       <div>
-        <p>코알라 서비스는..</p>
-        <p>캐러샐? 여기에 뭘 보여줘야 될지..</p>
+        <p>코알라 서비스 소개(위로 올릴지?)..</p>
+        <p>캐러셀 또는 카드로.</p>
       </div>
     </div>
   </div>
